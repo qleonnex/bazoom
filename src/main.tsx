d944bb80de6309c5f04498fbe40620446ce71969
wayne <qleonnex@gmail.com>
2025-01-30 10:12:56 +0500

    fix: provided all app in provider query-client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client"
+import "./index.css"
 
-import Main from "./components/main/Main.tsx";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Main from "./components/main/Main.tsx"
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <QueryClientProvider client={queryClient}>
     <Main />
-  </>,
+  </QueryClientProvider>,
 );
